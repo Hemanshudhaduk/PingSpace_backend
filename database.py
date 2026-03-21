db73a8afee127bf2dbb8f39f5079630df23238fa
= <=>
2026-03-21 21:10:15 +0530

    remove doteven

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
