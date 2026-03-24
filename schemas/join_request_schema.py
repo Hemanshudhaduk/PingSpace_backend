b7720d0caaf586e21b1d008ae36e797bffe9fa22
= <=>
2026-03-24 12:24:58 +0530

    done fix..

--- schemas/join_request_schema.py
+++ schemas/join_request_schema.py
@@ -2,8 +2,10 @@ from pydantic import BaseModel
 from datetime import datetime
 from schemas.user_schema import UserResponse
 
+# Add sender_user_id field
 class JoinRequestCreate(BaseModel):
     server_id: str
+    sender_user_id: str  # NEW: ID of user who sent the invite link
 
 class JoinRequestUpdate(BaseModel):
     status: str
