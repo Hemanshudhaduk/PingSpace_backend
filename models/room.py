5901a1c03b103d99d6b666a442969dde6af7425b
hemanshudhaduk22 <115709310+Hemanshudhaduk@users.noreply.github.com>
2026-04-30 12:50:18 +0530

    Update room.py

--- models/room.py
+++ models/room.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import relationship
 import uuid         
 class Room(Base) :
     __tablename__ = 'room'
-    id = Column(String , primary_key=True ,  default=lambda: str(uuid.uuid1()))
-    name = Column(String  , index=True)
-    description = Column(String , nullable=True)
-    server_id = Column(String ,ForeignKey('server.id'))
-    server = relationship('Server' , back_populates='room')
-    
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid1()))
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    visibility = Column(String, nullable=False, default="public")
+    server_id = Column(String, ForeignKey('server.id'))
+    server = relationship('Server', back_populates='room')
+    members = relationship('RoomMember', back_populates='room', cascade="all, delete-orphan")
